24d8ee4cbb2513d28d0f131a73e63efc8965614d
David Tyler <dave@bighairydave.com>
2013-09-03 14:11:01 +0100

    cunning reorganisation

--- asoiaf despoiler/myScript.js
+++
@@ -1,5 +0,0 @@
-var link = document.createElement("link");
-link.href = chrome.extension.getURL("asoiaf.css");
-link.type = "text/css";
-link.rel = "stylesheet";
-document.getElementsByTagName("head")[0].appendChild("link");
